5ec2001d3c21385ad242b6817ed10ccd18378db7
임성훈 <limpanda7@naver.com>
2023-02-01 00:36:58 +0900

    테스트 페이지 1차 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [address, setAddress] = useState('');
+  const [keys, setKeys] = useState(0);
+  const [form, setForm] = useState({
+    userId: '',
+    rank: '',
+    purchased: false
+  });
+
+  const {userId, rank, purchased} = form;
+
+  const postMessageToApp = (str) => {
+    window.ReactNativeWebView.postMessage(str);
+  }
+
+  useEffect(() => {
+    document.addEventListener('message', e => {
+      alert('받은 데이터: ' + e.data);
+    })
+  }, [])
+
+  const handleInput = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleCheckbox = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.checked,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => postMessageToApp('wallet')}>지갑연결</button><br/>
+        <span>연결된 지갑주소: {address}</span>
+      </div>
+
+      <hr/>
+
+      <div>
+        <button onClick={() => postMessageToApp('admob')}>광고시청</button><br/>
+        <span>열쇠 갯수: {keys}</span>
+      </div>
+
+      <hr/>
+
+      <div>
+        <button>아래 정보를 GA로 보내기</button>
+        <div>
+          <span style={{marginRight: '10px'}}>아이디</span>
+          <input name='userId' value={userId} onChange={handleInput}/>
+        </div>
+        <div>
+          <span style={{marginRight: '10px'}}>랭크</span>
+          <input name='rank' value={rank} onChange={handleInput}/>
+        </div>
+        <div>
+          <span style={{marginRight: '10px'}}>구매여부</span>
+          <input type='checkbox' name='purchased' checked={purchased} onChange={handleCheckbox}/>
+        </div>
+      </div>
+
     </div>
   );
 }
